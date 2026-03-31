653456f8176a6d4dd1a6be24d16ac914f9fe8053
nabilirzap-storywriter <nabilirianna@gmail.com>
2026-03-31 21:35:30 +0700

    Update preload.js

--- preload.js
+++ preload.js
@@ -1,7 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
 import { getFirestore, collection, addDoc, doc, updateDoc, onSnapshot, query, orderBy } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 
-// PASTE KODE FIREBASE KAMU DI SINI
 const firebaseConfig = {
   apiKey: "AIzaSyB_aesyRyMJftF1y9AQn6MiLWu0XTdKL4g",
   authDomain: "the-name-vault-d9a8f.firebaseapp.com",
@@ -11,9 +10,7 @@ const firebaseConfig = {
   appId: "1:607838251875:web:91862a55886277ce100156"
 };
 
-// Inisialisasi Database
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-// Export agar bisa dipakai oleh main.js
 export { db, collection, addDoc, doc, updateDoc, onSnapshot, query, orderBy };
